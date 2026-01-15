063191d4c12f404c23bfb1e83989964b11d07051
Luc Gaitskell <me@lucg.xyz>
2026-01-15 14:10:19 +0100

    Add systolic sorter

---
+++ hdl/systolic_sorter.sv
@@ -0,0 +1,100 @@
+
+localparam int INDEX_WIDTH = 32;
+
+typedef struct packed {
+  logic [INDEX_WIDTH-1:0] id1;
+  logic [INDEX_WIDTH-1:0] id2;
+} id_pair_s;
+
+module systolic_sorter #(
+    parameter int ELEMENTS = 64,
+    parameter int BIT_WIDTH = 32,
+    parameter type METADATA_TYPE = id_pair_s,
+    localparam int ADDRESS_WIDTH = $clog2(ELEMENTS)
+) (
+    input logic clk,
+    input logic rst,
+    input logic in_valid,
+    input logic [BIT_WIDTH-1:0] in_data,
+    input METADATA_TYPE in_metadata,
+    input logic in_last,
+    output logic out_last,
+    input logic [ADDRESS_WIDTH-1:0] out_address,
+    output logic out_valid,
+    output logic [BIT_WIDTH-1:0] out_data,
+    output METADATA_TYPE out_metadata
+);
+
+  logic array_valid[0:ELEMENTS-1];
+  logic [BIT_WIDTH-1:0] array_data[0:ELEMENTS-1];
+  id_pair_s array_metadata[0:ELEMENTS-1];
+  // the values moving are being propagated
+  //  note that it is one element larger to make my generator loops cleaner
+  //  (the last element should not be used)
+  logic array_valid_moving[0:ELEMENTS];
+  logic [BIT_WIDTH-1:0] array_data_moving[0:ELEMENTS];
+  id_pair_s array_metadata_moving[0:ELEMENTS];
+  logic last_moving[0:ELEMENTS];
+
+
+  always_comb begin : inputAssign
+    assign array_valid_moving[0] = in_valid;
+    assign array_data_moving[0] = in_data;
+    assign array_metadata_moving[0] = in_metadata;
+    assign last_moving[0] = in_last;
+  end
+
+  always_ff @(posedge clk) begin : lastLatch
+    if (rst) begin
+      out_last <= 1'b0;
+    end else if (last_moving[ELEMENTS]) begin
+      out_last <= last_moving[ELEMENTS];
+    end
+  end
+
+  // Input loading
+  always_ff @(posedge clk) begin : systolicArray
+    if (rst) begin
+      for (int i = 0; i < ELEMENTS; i++) begin
+        array_valid[i] <= 1'b0;
+      end
+    end else begin
+      for (int i = 0; i < ELEMENTS; i++) begin
+        last_moving[i+1] <= last_moving[i];
+        if (!array_valid[i]) begin
+          array_valid[i] <= array_valid_moving[i];
+          array_data[i] <= array_data_moving[i];
+          array_metadata[i] <= array_metadata_moving[i];
+
+          array_valid_moving[i+1] <= 1'b0;  // consumed
+        end else if (!array_valid_moving[i]) begin
+          // propagate invalid
+          array_valid_moving[i+1] <= array_valid_moving[i];
+        end else begin
+          // compare and swap
+          if (array_data_moving[i] < array_data[i]) begin
+            // moving value is smaller, swap
+            array_valid_moving[i+1] <= array_valid[i];
+            array_data_moving[i+1] <= array_data[i];
+            array_metadata_moving[i+1] <= array_metadata[i];
+
+            array_valid[i] <= array_valid_moving[i];
+            array_data[i] <= array_data_moving[i];
+            array_metadata[i] <= array_metadata_moving[i];
+          end else begin
+            // current value is smaller, propagate moving
+            array_valid_moving[i+1] <= array_valid_moving[i];
+            array_data_moving[i+1] <= array_data_moving[i];
+            array_metadata_moving[i+1] <= array_metadata_moving[i];
+          end
+        end
+      end
+    end
+  end
+
+  always_ff @(posedge clk) begin : outputSelect
+    out_valid <= array_valid[out_address];
+    out_data <= array_data[out_address];
+    out_metadata <= array_metadata[out_address];
+  end
+endmodule
---
+++ sim/test_systolic_sorter.py
@@ -0,0 +1,48 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles, RisingEdge, FallingEdge, ReadOnly
+import numpy as np
+from sim.lib import build_and_run_sim, reset
+
+
+NUM_ITER = 150
+C_S00_AXIS_TDATA_WIDTH = 32
+
+
+@cocotb.test
+async def test_a(dut):
+    test_data = [15, 15, 15, 15, 14, 11, 8, 9, 10, 8, 8, 6, 4, 2, 0, 2, 1, 3, 5, 7, 9]
+
+    cocotb.start_soon(Clock(dut.clk, 10, unit="ns").start())
+    await reset(dut.clk, dut.rst, 2)
+
+    for i, val in enumerate(test_data):
+        dut.in_data.value = val
+        # dut.in_metadata.value = i
+        dut.in_valid.value = 1
+        dut.in_last.value = int(i == len(test_data) - 1)
+        await ClockCycles(dut.clk, 1)
+    dut.in_valid.value = 0
+
+    await RisingEdge(dut.out_last)
+
+    for i, val in enumerate(sorted(test_data)):
+        dut.out_address.value = i
+        await RisingEdge(dut.clk)
+        await ReadOnly()
+        output_value = dut.out_data.value.to_unsigned()
+        # print(f"Got output value: {output_value}, expected {val}")
+        assert output_value == val, (
+            f"at index {i}, expected {val} but got {output_value}"
+        )
+        await FallingEdge(dut.clk)
+
+
+if __name__ == "__main__":
+    build_and_run_sim(
+        __file__,
+        hdl_toplevel="systolic_sorter",
+        parameters={
+            "BIT_WIDTH": 32,
+        },
+    )
